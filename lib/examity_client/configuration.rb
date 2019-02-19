47051c4039bdec3c68e898ec2292ebb424124015
Dusty Jones <dusty@trueability.com>
2019-02-19 15:16:49 -0600

    dont die in initialize

--- lib/examity_client/configuration.rb
+++ lib/examity_client/configuration.rb
@@ -6,9 +6,6 @@ class ExamityClient::Configuration
     @client_id = opts[:client_id]
     @secret_key = opts[:secret_key]
     @base_url = opts[:base_url]
-
-    raise "Client is not configured" unless @client_id.present? &&
-                                            @secret_key.present? &&
-                                            @base_url.present?
+    self
   end
 end
