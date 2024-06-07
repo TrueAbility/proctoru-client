2e41f2589e843ea66cbb79bd74bb83c78eed2eb5
sushweyi <su@aluminum.io>
2024-06-07 17:55:58 +0900

    fix: add sso_token test

--- test/sso_token_test.rb
+++ test/sso_token_test.rb
@@ -1,18 +1,36 @@
+require 'minitest/autorun'
+require 'webmock/minitest'
 require_relative "./test_helper"
 require "proctoru_client"
 
 class ProctoruClient::SsoTokenTest < Minitest::Test
   attr_accessor :client
   def setup
-    config = ProctoruClient::Configuration.new(
-      api_key: "cd8e357e-a608-4775-ac94-2db5e6b6f45e"
-      base_url: "https://demo.proctoru.com/api"
-    )
+    config = ProctoruClient::Configuration.new({
+        token: "your_api_key",
+        base_url: "https://demo.proctoru.com/api"
+    })
     @client = ProctoruClient::Client.new(config)
   end
 
-  def test_can_generate_sso_token
-    token = client.sso_token('user@example.com')
-    assert_equal "Xgc+7jn/WDAEfo4kzW8ON2CL/v2yRvyiaI+wjrnLOvc=", token
+  def test_initialize
+    assert_equal "https://demo.proctoru.com/api", @client.config.base_url
+    assert_equal "your_api_key", @client.config.token
+  end
+
+  def test_sso_token_success
+    stub_request(:post, 'https://demo.proctoru.com/api/api/autoLogin').with(body: { email: 'test@example.com' }.to_json,
+                        headers: { 
+                            'Authorization-Token' => 'your_api_key', 
+                            'Content-Type' => 'application/x-www-form-urlencoded' 
+                          }).to_return(status: 200, body: {
+                          "time_sent": "2024-06-07T08:28:14Z",
+                          "response_code": 1,
+                          "message": "",
+                          "data": "your_sso_token"
+                        }.to_json)
+
+    sso_token = @client.sso_token('test@example.com')  
+    assert_equal 'your_sso_token', sso_token
   end
 end
